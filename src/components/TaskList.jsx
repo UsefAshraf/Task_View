438e257229935515464ab69da78ab0bf3f2971b2
UsefAshraf <yousashrafef1247@gmail.com>
2025-09-18 21:41:25 +0300

    Update: fixed task toggle & added toast notification

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -41,7 +41,7 @@ const TaskList = () => {
   if (isLoading) {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 flex items-center justify-center">
-      <Spinner />
+      <p>Loading...</p>
     </div>
   );
 }
